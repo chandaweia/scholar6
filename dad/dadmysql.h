06364f7f68a37594f30291b2437a2146c1b08f67
root <root@n.edu.cn>
2018-07-14 15:34:20 +0800

    delete dad

--- dad/dadmysql.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DADMYSQL_H_
-#define _DADMYSQL_H_
-
-//extern MYSQL mysql;
-int dad_mysql_login(const char*host, const char* user, const char* passwd, const char*db);
-void dad_mysql_close();
-char* dad_mysql_get_user(unsigned char* mac);
-
-#endif
